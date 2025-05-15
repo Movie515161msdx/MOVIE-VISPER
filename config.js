fa74dbed54d40a0b0ec8747b09f5a23a440610e9
Movie515161msdx <ginhanmanula@gmail.com>
2025-05-15 14:07:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'put session id' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=O4wyVJDQ#IP2pkdR9LN-ZVpD-YbYvJfrM-YNQjZuY6EJIJYSbSF8' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'movievisper' : process.env.SESSION_NAME,  
 
